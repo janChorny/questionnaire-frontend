375b74fecda486beab80600534fcc51183dc07e7
janChorny <janchorny@tut.by>
2023-10-25 20:38:43 +0300

    feat: add preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,45 @@
+import React, {Suspense, lazy} from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Public from './components/Public'
-import Login from './features/auth/Login';
 import Prefetch from './features/auth/Prefetch'
-import Instructions from './components/Instructions';
-import Questionnaire from './components/Questionnaire';
-import ThxPage from './components/ThxPage';
 import ResultsList from './components/Results';
-import PatientOne from './components/PatientOne';
-import PatientTwo from './components/PatientTwo';
-import PatientThree from './components/PatientThree';
-import PatientFour from './components/PatientFour';
-import PatientFive from './components/PatientFive';
+const Layout= lazy(() => import('./components/Layout'));
+const Public = lazy(() => import('./components/Public'));
+const Login = lazy(() => import('./features/auth/Login'));
+const Instructions  = lazy(() => import('./components/Instructions'));
+const Questionnaire  = lazy(() => import('./components/Questionnaire'));
+const ThxPage  = lazy(() => import('./components/ThxPage'));
+const PatientTwo= lazy(() => import('./components/PatientTwo'));
+const PatientThree= lazy(() => import('./components/PatientThree'));
+const PatientFour= lazy(() => import('./components/PatientFour'));
+const PatientFive= lazy(() => import('./components/PatientFive'));
+const PatientOne= lazy(() => import('./components/PatientOne'));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Public />} />
-        <Route path="public" index element={<Public />} />
-        <Route path="login" element={<Login />} />
-        <Route path="instructions" element={<Instructions/>} />
-        <Route path="patient-one" element={<PatientOne/>} />
-        <Route path="patient-two" element={<PatientTwo/>} />
-        <Route path="patient-three" element={<PatientThree/>} />
-        <Route path="patient-four" element={<PatientFour/>} />
-        <Route path="patient-five" element={<PatientFive/>} />
-        <Route path="questionnaire" element={<Questionnaire/>} />
-        <Route path="thxPage" element={<ThxPage/>} />
-
-        <Route element={<Prefetch />}>
-            <Route path="notes">
-              <Route index element={<ResultsList />} />
-            </Route>
+    <Suspense fallback={<div className="loader"></div>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Public />} />
+          <Route path="public" index element={<Public />} />
+          <Route path="login" element={<Login />} />
+          <Route path="instructions" element={<Instructions/>} />
+          <Route path="patient-one" element={<PatientOne/>} />
+          <Route path="patient-two" element={<PatientTwo/>} />
+          <Route path="patient-three" element={<PatientThree/>} />
+          <Route path="patient-four" element={<PatientFour/>} />
+          <Route path="patient-five" element={<PatientFive/>} />
+          <Route path="questionnaire" element={<Questionnaire/>} />
+          <Route path="thxPage" element={<ThxPage/>} />
+          <Route element={<Prefetch />}>
+              <Route path="notes">
+                <Route index element={<ResultsList />} />
+              </Route>
+          </Route>
+  
         </Route>
+      </Routes>
+    </Suspense>
 
-      </Route>
-    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,13 @@ import { store } from './app/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+  <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
   </React.StrictMode>,
 )
