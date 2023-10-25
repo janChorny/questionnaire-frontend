31558843a9b3b2a15ac0764e1374410f8c85744d
janChorny <janchorny@tut.by>
2023-10-25 20:57:09 +0300

    feat: add change to first deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
